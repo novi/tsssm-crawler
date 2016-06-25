0656865f7f645b1f2c6149e0bfb9ff5af22eb573
Yusuke Ito <novi.mad@gmail.com>
2016-06-26 08:55:22 +0900

    add Manager app project (WIP)

---
+++ Xcode/RSSManager/RSSManager/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RSSManager
+//
+//  Created by Yusuke Ito on 6/26/16.
+//  Copyright © 2016 Yusuke Ito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ notification: NSNotification) {
+        
+    }
+    
+    func applicationShouldTerminate(afterLastWindowClosed sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
+
---
+++ Xcode/RSSManager/RSSManager/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RSSManager
+//
+//  Created by Yusuke Ito on 6/26/16.
+//  Copyright © 2016 Yusuke Ito. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
