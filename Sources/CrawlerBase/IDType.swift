b8d505faf61407eea4459d25fca7f4e2641340e6
Yusuke Ito <novi.mad@gmail.com>
2016-06-27 20:21:11 +0900

    fix

--- Sources/CrawlerBase/IDType.swift
+++ Sources/CrawlerBase/IDType.swift
@@ -24,49 +24,4 @@ public struct ArticleID: IDType {
     }
 }
 
-public enum AutoincrementID<I: IDType> {
-    case noID
-    case ID(I)
-    
-    public var id: I {
-        switch self {
-        case .noID: fatalError("has no ID")
-        case .ID(let id): return id
-        }
-    }
-    
-    mutating func replaceWith(id: I) {
-        self = .ID(id)
-    }
-}
 
-extension AutoincrementID: CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .noID: return "noID"
-        case .ID(let id): return "\(id.id)" // TODO: use description
-        }
-    }
-}
-
-
-extension AutoincrementID: SQLStringDecodable {
-    public static func from(string: String) -> AutoincrementID<I>? {
-        guard let id = I.from(string: string) else {
-            return nil
-        }
-        return AutoincrementID(id)
-    }
-    public init(_ id: I) {
-        self = .ID(id)
-    }
-}
-
-extension AutoincrementID: QueryParameter {
-    public func queryParameter(option: QueryParameterOption) throws -> QueryParameterType {
-        switch self {
-        case .noID: fatalError("TODO")
-        case .ID(let id): return try id.queryParameter(option: option)
-        }
-    }
-}
